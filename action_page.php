eb4a1d642415cdd0a9b0db2d431ecd1751678534
Ruiyi <ryzhou@uw.edu>
2015-02-14 00:17:34 -0800

    02/14 version

---
+++ action_page.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+</body>
+</html>
---
+++ feelcolor.js
@@ -0,0 +1,9 @@
+  var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+alert( rgbToHex(0, 51, 255) ); 
