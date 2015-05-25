3f8bc3d4cf0185d412751d326299f7996fc88774
Ruiyi <ryzhou@uw.edu>
2015-05-25 15:29:55 -0700

    508finalassignment-May25th

---
+++ animation.js
@@ -0,0 +1,17 @@
+    
+$("#backtotop a[href^='#']").on('click', function(e) {
+
+       // prevent default anchor click behavior
+       e.preventDefault();
+
+       // animate
+       $('html, body').animate({
+           scrollTop: $(this.hash).offset().top
+         }, 300, function(){
+   
+           // when done, add hash to url
+           // (default click behaviour)
+           window.location.hash = this.hash;
+         });
+
+    });
