9b64e9213ceb3fd3b4a15f280b69ee1e3c960e5d
Ruiyi <ryzhou@uw.edu>
2016-01-07 22:28:51 -0800

    Delete unused files.

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" type="text/css" href="bootstrap-3.3.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="bootstrap-3.3.2/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="bootstrap-3.3.2/css/animate.css">
-    <link rel="stylesheet" type="text/css" href="css/agency.css">
     <link rel="stylesheet" type="text/css" href="bootstrap-3.3.2/css/personal.css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="bootstrap-3.3.2/js/bootstrap.min.js"></script>
@@ -21,7 +20,6 @@
     <script src="js/modernizr.custom.97074.js"></script>
     <script src="js/scrollbacktotop.js"></script> 
     <!-- Parallax Scrolling -->
-<script src="js/parallax.js"></script>
 <script src="js/parallax.min.js"></script>
 <script src="js/scrollhidden.js"></script>
       <!-- End -->
