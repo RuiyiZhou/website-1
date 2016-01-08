a3cfe9212054c008e1367aff6336acf9c77b9a2d
Ruiyi <ryzhou@uw.edu>
2016-01-07 19:31:52 -0800

    Update whatelse and book gallery section

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
    
 <?php include('header.php'); ?>
 
+    <!--Blue Image Gallery-->
+<link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+<link rel="stylesheet" href="css/bootstrap-image-gallery.css">
+
 <title>Ruiyi Zhou | UX/UI Designer</title>
     <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -160,32 +164,141 @@
        
       </div>
       
-      
-      
-      
          </div>
          </section>
+
+<section id="whatelse">
+
+     <div class="container">
+        <div class="margin15">
+     <h3 class="bs-docs-featurette-title nextproject ">I'm also a(n)</h3>
+
+      <hr class="featurette-divider">
+
+      &nbsp;
+      &nbsp;
+      <div class="row text-center ">
+
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/avidreader.png" alt="Generic placeholder image" style="width: 100px; height: 100px;">
+          <h3>Avid Reader</h3>
+        </div><!-- /.col-lg-4 -->
+
+          <div class="col-lg-4">
+             <img class="img-circle center" src="images/selftaughteconomist.png" alt="Generic placeholder image" style="width: 100px; height: 100px;">
+          
+        <h3>Self-taught Economist</h3>
+        </div><!-- /.col-lg-4 -->
+
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/halfbloodgeek.png" alt="Generic placeholder image" style="width: 100px; height: 100px;">
+          <h3>Half-blood Geek</h3>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+         </div>
+         </div>
+     
+      &nbsp;
+      &nbsp;
+    </section>
 
       <section id="What's New">
      <div class="container">
     <div class="margin15">
-<h3 class="bs-docs-featurette-title nextproject ">What's New</h3>
+
+
+<h3 class="bs-docs-featurette-title nextproject ">Book Gallery</h3>
  <hr class="featurette-divider">
+
+
+<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
+<div id="blueimp-gallery" class="blueimp-gallery">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <!-- Controls for the borderless lightbox -->
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <a class="play-pause"></a>
+    <ol class="indicator"></ol>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->
+    <div class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body next"></div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left prev">
+                        <i class="glyphicon glyphicon-chevron-left"></i>
+                        Previous
+                    </button>
+                    <button type="button" class="btn btn-primary next">
+                        Next
+                        <i class="glyphicon glyphicon-chevron-right"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
   <div class="row">
-  <div class="col-md-5">
-  <h3>Right now I'm reading</h3>
-  <a href="https://www.goodreads.com/user/show/47665326-ruiyi-zhou" target="_blank"><img class="featurette-image img-responsive img-inlineblock opacity" src="images/logo_goodreads.png" alt="Generic placeholder image" style="max-width:100px; "></a>
-  </div>
-  <div class="col-mg-7">
-  <a href="https://www.goodreads.com/book/show/99315.A_Whole_New_Mind" target="_blank"><img class="featurette-image img-responsive img-inlineblock" src="images/book_awholenewmind.jpg" alt="Generic placeholder image" style="max-width:150px; "></a>
+<div id="links">
 
-  <a href="https://www.goodreads.com/book/show/69242.Made_to_Stick" target="_blank"><img class="featurette-image img-responsive img-inlineblock" src="images/book_madetostick.jpeg" alt="Generic placeholder image" style="max-width:150px; "></a>
+    <a href="images/book_madetostick.jpeg" title="Made To Stick" data-gallery>
+        <img class="small75" src="images/book_madetostick.jpeg" alt="Made To Stick">
+    </a>
+    <a href="https://d.gr-assets.com/books/1442616448l/41597.jpg" title="The Non-Designer's Design Book" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1442616448l/41597.jpg" alt="The Non-Designer's Design Book">
+    </a>
+    <a href="https://d.gr-assets.com/books/1333124707l/13539381.jpg" title="How China Became Capitalist" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1333124707l/13539381.jpg" alt="How China Became Capitalist">
+    </a>
+    <a href="https://d.gr-assets.com/books/1410138134l/10639.jpg" title="The Paradox of Choice" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1410138134l/10639.jpg" alt="The Paradox of Choice">
+    </a>  
+    <a href="images/book_awholenewmind.jpg" title="A Whole New Mind" data-gallery>
+        <img class="small75" src="images/book_awholenewmind.jpg" alt="A Whole New Mind">
+    </a>      
+    <a href="https://d.gr-assets.com/books/1347488145l/6659874.jpg" title="Behind the Cloud" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1347488145l/6659874.jpg" alt="Behind the Cloud">
+    </a>   
+    <a href="https://d.gr-assets.com/books/1404576602l/13099738.jpg" title="Steal Like An Artist" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1404576602l/13099738.jpg" alt="Steal Like An Artist">
+    </a>        
+    <a href="https://d.gr-assets.com/books/1347329018l/980280.jpg" title="Sketching User Experience" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1347329018l/980280.jpg" alt="Sketching User Experience">
+    </a>       
+    <a href="https://d.gr-assets.com/books/1365396257l/6157547.jpg" title="Interdisciplinary Interaction Design" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1365396257l/6157547.jpg" alt="Interdisciplinary Interaction Design">
+    </a> 
+    <a href="https://d.gr-assets.com/books/1328834536l/336258.jpg" title="Information Dashboard Design" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1328834536l/336258.jpg" alt="Information Dashboard Design">
+    </a>   
+    <a href="https://d.gr-assets.com/books/1387739935l/19904558.jpg" title="Service Design" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1387739935l/19904558.jpg" alt="Service Design">
+    </a>          
+    <a href="https://d.gr-assets.com/books/1388211043l/68143.jpg" title="The Wisdom of Crowds" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1388211043l/68143.jpg" alt="The Wisdom of Crowds">
+    </a>   
+    <a href="https://d.gr-assets.com/books/1266606574l/646060.jpg" title="Worlds Together, Worlds Apart" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1266606574l/646060.jpg" alt="Worlds Together, Worlds Apart">
+    </a>  
+    <a href="https://d.gr-assets.com/books/1366758683l/12609433.jpg" title="The Power of Habit" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1366758683l/12609433.jpg" alt="The Power of Habit">
+    </a>  
+    <a href="https://d.gr-assets.com/books/1387365289l/18324256.jpg" title="Qualitative Research Practice" data-gallery>
+        <img class="small75" src="https://d.gr-assets.com/books/1387365289l/18324256.jpg" alt="Qualitative Research Practice">
+    </a>  
 
-  <a href="https://www.goodreads.com/book/show/336258.Information_Dashboard_Design" target="_blank"><img class="featurette-image img-responsive img-inlineblock" src="images/book_informationdashboarddesign.png" alt="Generic placeholder image" style="max-width:150px; "></a>
+</div>
+</div>
+
   </div>
-  </div>
-  </div>
-  </div>
+</div>
   </section>
 
       
@@ -194,6 +307,10 @@
 
         
 <?php include('footer.php'); ?>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+<script src="js/bootstrap-image-gallery.js"></script>
 
 </body>
 </html>
